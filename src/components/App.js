c407a445fa0468f50ddacc7d3ee44ba93e69e554
haind <nduchai1998@gmail.com>
2024-11-04 09:55:51 +0700

    feat: delete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,9 @@
-import { useEffect, Fragment } from "react";
+import { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
-import { handleInitialData } from "../actions/shared";
-import Dashboard from "./Dashboard";
 import LoadingBar from "react-redux-loading-bar";
-import NewTweet from "./NewTweet";
-import TweetPage from "./TweetPage";
-import Nav from "./Nav";
+import { handleInitialData } from "../actions/shared";
 import Login from "./Login";
-import { Routes, Route } from "react-router-dom";
+import Nav from "./Nav";
 
 const App = (props) => {
   useEffect(() => {
@@ -20,14 +16,6 @@ const App = (props) => {
       <Nav />
       <div className="container">
         <Login />
-        {/* <Nav />
-        {props.loading === true ? null : (
-          <Routes>
-            <Route path="/" exact element={<Dashboard />} />
-            <Route path="/tweet/:id" element={<TweetPage />} />
-            <Route path="/new" element={<NewTweet />} />
-          </Routes>
-        )} */}
       </div>
     </Fragment>
   );
