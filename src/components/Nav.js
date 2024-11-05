aa10ba87621cf61047645599a3f28d74903d50c6
haind <nduchai1998@gmail.com>
2024-11-05 13:56:15 +0700

    link

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
+import { Routes, Route,useLocation } from "react-router-dom";
 
 const Nav = () => {
+  const location = useLocation();
   return (
     <nav className="nav d-flex justify-space">
       <ul className="ms-2">
-        <li>
+        <li className={
+          location.pathname !== '/' ?
+          'active-nav' : ''
+        }>
           <Link to="/">Home</Link>
         </li>
-        <li>
-          <Link to="/new">Leaderboard</Link>
+        <li className={
+          location.pathname !== '/board' ?
+          'active-nav' : ''
+        }>
+          <Link to="/board">Leaderboard</Link>
         </li>
-        <li>
+        <li className={
+          location.pathname !== '/new' ?
+          'active-nav' : ''
+        }>
           <Link to="/new">New</Link>
         </li>
       </ul>
